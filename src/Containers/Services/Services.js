157ad240f4bf580fafb48609a910f2b167792541
Jawz0805 <67982917+Jawz0805@users.noreply.github.com>
2021-11-14 14:21:00 +0000

    responsive text updates

--- src/Containers/Services/Services.js
+++ src/Containers/Services/Services.js
@@ -6,7 +6,7 @@ import ServiceImage from '../../Components/ServiceImage/ServiceImage';
 import jedd from '../../Assets/jeddstill.jpg'
 import performance from '../../Assets/NiiPerformance-2.jpg'
 import sm7b1 from '../../Assets/SM7b-2.jpg'
-import DT7701 from '../../Assets/DT770.jpg'
+import DT7701 from '../../Assets/DT770-1.jpg'
 
 
 function Services
@@ -14,10 +14,10 @@ function Services
     return (
         <div className="services-container" id="services">
             <h2 style={{ textAlign: "center" }}>Services and Equipment</h2>
-            <ServiceImage image={jedd} title="Podcast Recording" price="From £80" text="Professional podcast audio recording and editing with the assistance of our in-house engineers. We also have packages that include video podcast recording using our Black Magic cinema cameras. " flex="row" />
-            <ServiceImage image={performance} price="From £80" title="Vocal and Acoustic Recording" text="Audio and video production of vocal and acoustic performances with top of the range equipment in our homely studio." flex="row-reverse" />
-            <ServiceImage image={sm7b1} price="From £20 P/H" title="Voice Over Recording" text="Our soundproofed studio makes it the perfect environment to record great sounding vocals and voice-overs. Whether you’re working on an advert, or looking to promote your business, we can help you achieve the quality you need for specification." flex="row" />
-            <ServiceImage image={DT7701} price="From £20 P/H" title="Mixing, Production and Mastering" text="Mixing and mastering for all forms of audio including voice overs, music and podcasts" flex="row-reverse" />
+            <ServiceImage image={jedd} alt="podcast table recrooms studios" title="Podcast Recording" price="From £80" text="Professional podcast audio recording and editing with the assistance of our in-house engineers. We also have packages that include video podcast recording using our Black Magic cinema cameras. " flex="row" />
+            <ServiceImage image={performance} alt="Live acoustic performance recording" price="From £80" title="Vocal and Acoustic Recording" text="Audio and video production of vocal and acoustic performances with top of the range equipment in our homely studio." flex="row-reverse" />
+            <ServiceImage image={sm7b1} alt="voice over recording shure sm7b" price="From £20 P/H" title="Voice Over Recording" text="Our soundproofed studio makes it the perfect environment to record great sounding vocals and voice-overs. Whether you’re working on an advert, or looking to promote your business, we can help you achieve the quality you need for specification." flex="row" />
+            <ServiceImage image={DT7701} alt="professional studio editing, mixing, mastering Beyer Dynamic DT770 Headphones" price="From £20 P/H" title="Mixing, Production and Mastering" text="Mixing and mastering for all forms of audio including voice overs, music and podcasts" flex="row-reverse" />
 
             {/* <a href={Pricelist} target="_blank" rel="noreferrer" className="pricelist-link">DOWNLOAD OUR PRICELIST</a> */}
 
