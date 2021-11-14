a103b88aca26a4bba1f3ac20bbb73b60a6d1586b
Jawz0805 <67982917+Jawz0805@users.noreply.github.com>
2021-11-14 17:58:13 +0000

    background image fixed

--- src/Components/HeroImage/HeroImage.js
+++ src/Components/HeroImage/HeroImage.js
@@ -4,7 +4,7 @@ import './_HeroImage.scss'
 const HeroImage = ({ image, text1, text2, id }) => {
     return (
 
-        <div className="hero-image" id={id} style={{ backgroundImage: `url(${image})` }}>
+        <div className="hero-image" id={id} >
             <div className="hero-text-container">
                 <h1 className="hero-title">{text1}</h1>
                 <h2 className="hero-title2">{text2}</h2>
